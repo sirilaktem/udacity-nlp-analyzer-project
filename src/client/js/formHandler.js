328ce4c50994ff7a241488bf733fb6b3650f291c
sirilaktem <sirilaktem@outlook.com>
2023-06-11 16:17:11 -0700

    Integrate API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,97 @@
-function handleSubmit(event) {
+const handleSubmit = (event) => {
     event.preventDefault();
 
+    const resultHolder = document.getElementById('results');
     // check what text was put into the form field
-    let formText = document.getElementById('name').value;
-    checkForName(formText);
+    let formText = document.getElementById('name').value.trim();
+    let textType = checkValidUrl(formText) ? 'URL' : 'TEXT';
 
-    console.log('::: Form Submitted :::');
-    fetch('http://localhost:8080/test')
-        .then((res) => res.json())
-        .then(function (res) {
-            document.getElementById('results').innerHTML = res.message;
+    if (checkEmptyInput(formText)) {
+        postData('/nlp', { term: formText, type: textType }).then((result) => {
+            resultHolder.innerHTML = formatResult(result, formText, textType);
         });
-}
+    }
+};
+
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    try {
+        const newData = await response.json();
+        //console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+const formatResult = (data, inputVal, inputType) => {
+    result =
+        '<strong class="result-head">Text/URL: </strong><span>' +
+        inputVal +
+        '</span><br>';
+    result +=
+        '<strong class="result-head">Type: </strong><span>' +
+        inputType +
+        '</span><br>';
+    if (data.status.code === '0') {
+        result +=
+            '<strong class="result-head">Polarity: </strong><span>' +
+            translatePolarity(data.score_tag) +
+            '</span><br>';
+        result +=
+            '<strong class="result-head">Subjectivity: </strong><span>' +
+            data.subjectivity +
+            '</span><br>';
+        result +=
+            '<strong class="result-head">Agreement: </strong><span>' +
+            data.agreement +
+            '</span><br>';
+        result +=
+            '<strong class="result-head">Confidence: </strong><span>' +
+            data.confidence +
+            '</span class="result-head"></br>';
+        result +=
+            '<strong class="result-head">Irony: </strong><span>' +
+            data.irony +
+            '</span></br>';
+    } else {
+        result +=
+            '<strong class="result-head">Error: </strong><span>' +
+            data.status.msg +
+            '</span></br>';
+    }
+    return result;
+};
+
+const translatePolarity = (score) => {
+    switch (score) {
+        case 'P+':
+            polarity = 'STRONG POSITIVE';
+            break;
+        case 'P':
+            polarity = 'POSITIVE';
+            break;
+        case 'NEU':
+            polarity = 'NEUTRAL';
+            break;
+        case 'N':
+            polarity = 'NEGATIVE';
+            break;
+        case 'N+':
+            polarity = 'STRONG NEGATIVE';
+            break;
+        case 'NONE':
+            polarity = 'WITHOUT POLARITY';
+            break;
+        default:
+            polarity = 'N/A';
+    }
+    return polarity;
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,57 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
 const mockAPIResponse = require('./mockAPI.js');
-const port = 8080;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT;
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('src/client'));
 
 app.get('/', (req, res) => {
     res.sendFile('/client/views/index.html', { root: __dirname + '/..' });
 });
 
+// designates what port the app will listen to for incoming requests
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Your API key is ${process.env.API_KEY}`);
 });
 
 app.get('/test', (req, res) => {
     res.send(mockAPIResponse);
+});
+
+const getAnalyzedData = async (params) => {
+    //console.log(params.term);
+    const inputType = params.type === 'URL' ? 'url' : 'txt';
+    const response = await fetch(
+        'https://api.meaningcloud.com/sentiment-2.1?key=' +
+            process.env.API_KEY +
+            '&lang=auto&' +
+            inputType +
+            '=' +
+            encodeURIComponent(params.term)
+    );
+
+    try {
+        const data = await response.json();
+        //console.log(data);
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+app.post('/nlp', (req, res) => {
+    const params = req.body;
+    console.log(params);
+    getAnalyzedData(params).then((data) => {
+        console.log(data);
+        res.send(data);
+    });
 });
